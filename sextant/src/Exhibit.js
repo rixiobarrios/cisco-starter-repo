1a07c2131659f50964a63a4f61c1d0ce6ba786e8
rixiobarrios <rixiobarrios.consulado@gmail.com>
2023-08-03 14:54:37 -0500

    Added two components

---
+++ sextant/src/Exhibit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exhibit = () => {
+    return (
+        <header>
+            <p>This React App Sextant is running...</p>
+        </header>
+    );
+};
+
+export default Exhibit;
